d360d5283710f9dbb03cb44418e408346b8f840a
M16A4PAPER <m16a4paper@gmail.com>
2025-02-23 15:16:25 +0500

    StudentManagement.Maui Project Added

---
+++ StudentManagement.MAUI/StudentManagement.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentManagement.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
