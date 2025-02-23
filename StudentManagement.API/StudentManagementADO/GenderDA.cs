39561574d235ed5bb6454a1162ed60525cb93676
M16A4PAPER <m16a4paper@gmail.com>
2025-02-23 21:46:24 +0500

    Get Gender By Id Store Procedure Changed

--- StudentManagement.API/StudentManagementADO/GenderDA.cs
+++ StudentManagement.API/StudentManagementADO/GenderDA.cs
@@ -39,5 +39,37 @@ namespace StudentManagement.ADODAL
 
             return genders;
         }
+
+        public List<Gender> GetGenderByID(int genderID)
+        {
+            List<Gender> gender = new List<Gender>();
+
+            using (SqlConnection connection = DBConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("GetGenderByID", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    //Add the parameter
+                    command.Parameters.Add(new SqlParameter("GenderId", System.Data.SqlDbType.Int)
+                    {
+                        Value = genderID
+                    });
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                            }
+                        }
+                    }
+                }
+
+            }
+            return gender;
+        }
     }
 }
