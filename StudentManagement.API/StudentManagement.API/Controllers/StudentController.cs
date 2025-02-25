02a60097eff4b590501a13a8b97a6017049d5ac9
M16A4PAPER <m16a4paper@gmail.com>
2025-02-25 21:48:22 +0500

    Revert "Update method now uses a Stored Procedure"
    This reverts commit 60ab421d86aa3da83d7c55d5b8ea2d9e6bad7869.

--- StudentManagement.API/StudentManagement.API/Controllers/StudentController.cs
+++ StudentManagement.API/StudentManagement.API/Controllers/StudentController.cs
@@ -37,20 +37,19 @@ namespace StudentManagement.API.Controllers
         }
 
         [HttpPut("UpdateStudent")]
-        public ActionResult UpdateStudent([FromBody] Student student)
+        public ActionResult UpdateStudent(
+            [FromQuery] int studentId,
+            [FromQuery] string name,
+            [FromQuery] int genderID,
+            [FromQuery] DateTime dateOfBirth,
+            [FromQuery] decimal height,
+            [FromQuery] int weight)
         {
-            if (student == null || student.ID <= 0)
-            {
-                return BadRequest(new { message = "Student ID is required for update." }); //Because Ids aren't auto assigned
-            }
-
             var studentDA = new StudentDA();
-            studentDA.UpdateStudent(student);
+            studentDA.UpdateStudent(studentId, name, genderID, dateOfBirth, height, weight);
 
             return Ok(new { message = "Student updated successfully." });
         }
-
-
 
         [HttpDelete("DeleteStudent")]
         public ActionResult DeleteStudent([FromQuery] int studentId)
