8ec8b5a577c492e8a9c9767842af35801e01d321
M16A4PAPER <m16a4paper@gmail.com>
2025-02-27 19:42:37 +0500

    Gender View UI Created

---
+++ StudentManagement.MAUI/StudentManagement.MAUI/Views/GenderView.xaml.cs
@@ -0,0 +1,15 @@
+namespace StudentManagement.MAUI.Views;
+
+public partial class GenderView : ContentPage
+{
+    public GenderView()
+    {
+        InitializeComponent();
+        LoadGenderIds();
+    }
+
+    private void LoadGenderIds()
+    {
+        
+    }
+}
---
+++ StudentManagement.MAUI/StudentManagement.MAUI/Views/StudentView.xaml.cs
@@ -0,0 +1,9 @@
+namespace StudentManagement.MAUI.Views;
+
+public partial class StudentView : ContentPage
+{
+	public StudentView()
+	{
+		InitializeComponent();
+	}
+}
