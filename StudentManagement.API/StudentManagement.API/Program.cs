085e2a1e341d1c744376b6aa8f581f3bd137dcd8
M16A4PAPER <m16a4paper@gmail.com>
2025-03-03 17:23:30 +0500

    Api Preotection w Middleware

--- StudentManagement.API/StudentManagement.API/Program.cs
+++ StudentManagement.API/StudentManagement.API/Program.cs
@@ -1,3 +1,4 @@
+using StudentManagement.API;
 using StudentManagement.API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.AddSwaggerApiKeySecurity());
 
 var app = builder.Build();
 
@@ -20,6 +21,7 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseApiKeyMiddleware();
 
 app.UseAuthorization();
 
