a279ae7523aedbff25a4a8eed9d78991d20e74de
M16A4PAPER <m16a4paper@gmail.com>
2025-04-01 15:38:16 +0500

    JS Fundamentals - Modules 7/10

--- JavascriptPractice/ProjectFiles/index.js
+++ JavascriptPractice/ProjectFiles/index.js
@@ -85,75 +85,134 @@ let employees = [
 import createPrompt from 'prompt-sync';
 let prompt = createPrompt();
 
-const command = process.argv[2] ? process.argv[2].toLowerCase() : null;
-
-if (!command) {
-  console.error("Error: No command provided. Use 'list' or 'add'.");
-  process.exit(1);
+const logEmployee = (employee) => {
+  Object.entries(employee).forEach(entry => {
+    console.log(`${entry[0]}: ${entry[1]}`);
+  });
 }
 
-switch (command) {
-  case 'list':
-    console.log(`Employee List ----------------------------\n`);
-    for (let emp of employees) {
-      for (let property in emp) {
-        console.log(`${property}: ${emp[property]}`);
-      }
-      console.log('');
-      prompt('Press enter to continue...');
+function getInput(promptText, validator, transformer) {
+  let value = prompt(promptText);
+  if (validator && !validator(value)) {
+    console.error(`--Invalid input`);
+    return getInput(promptText, validator, transformer);
+  }
+  if(transformer) {
+    return transformer(value);
+  }
+  return value;
+}
+
+// Validator functions ---------------------------------------------------
+
+const isStringInputValid = (input) => {
+  return (input) ? true : false;
+}
+
+const isBooleanInputValid = function (input) {
+  return (input === "yes" || input === "no");
+}
+
+const isIntegerValid = (min, max) => {
+  return (input) => {
+    let numValue = Number(input);
+    if (!Number.isInteger(numValue) || numValue < min || numValue > max) {
+      return false;
     }
-    console.log(`Employee list completed`);
+    return true;
+  }
+}
+
+// Application commands --------------------------------------------------
+
+function listEmployees() {
+  console.log(`Employee List ----------------------------`);
+  console.log('');
+
+  employees.forEach(e => {
+    logEmployee(e);
+    prompt('Press enter to continue...');
+  });
+  console.log(`Employee list completed`);
+}
+
+function addEmployee() {
+  console.log(`Add Employee -----------------------------`);
+  console.log('');
+  let employee = {};
+  employee.firstName = getInput("First Name: ", isStringInputValid);
+  employee.lastName = getInput("Last Name: ", isStringInputValid);
+  let startDateYear = getInput("Employee Start Year (1990-2023): ", isIntegerValid(1990, 2023));
+  let startDateMonth = getInput("Employee Start Date Month (1-12): ", isIntegerValid(1, 12));
+  let startDateDay = getInput("Employee Start Date Day (1-31): ", isIntegerValid(1, 31));
+  employee.startDate = new Date(startDateYear, startDateMonth - 1, startDateDay);
+  employee.isActive = getInput("Is employee active (yes or no): ", isBooleanInputValid, i => (i === "yes"));
+  
+  // Output Employee JSON
+  const json = JSON.stringify(employee, null, 2);
+  console.log(`Employee: ${json}`);
+}
+
+// Search for employees by id
+function searchById() {
+ const id = getInput("Employee ID: ", null, Number);
+ const result = employees.find(e => e.id === id);
+ if (result) {
+    console.log("");
+    logEmployee(result);
+  } else {
+    console.log("No results...");
+  }
+}
+
+// Search for employees by name
+function searchByName() {
+  const firstNameSearch = getInput("First Name: ").toLowerCase();
+  const lastNameSearch = getInput("Last Name: ").toLowerCase();
+  const results = employees.filter(e => {
+    if (firstNameSearch && !e.firstName.toLowerCase().includes(firstNameSearch)) {
+      return false;
+    }
+    if (lastNameSearch && !e.lastName.toLowerCase().includes(lastNameSearch)) {
+      return false;
+    }
+    return true;
+  });
+  results.forEach((e, idx) => {
+    console.log("");
+    console.log(`Search Result ${idx + 1} -------------------------------------`);
+    logEmployee(e);
+  });
+}
+
+// Application execution -------------------------------------------------
+
+// Get the command the user wants to exexcute
+const command = process.argv[2].toLowerCase();
+
+switch (command) {
+
+  case 'list':
+    listEmployees();
     break;
 
   case 'add':
-    console.log(`Add Employee -----------------------------\n`);
-    let employee = {};
+    addEmployee();
+    break;
 
-    let firstName = prompt("First Name: ");
-    if (!firstName) {
-      console.error(`Invalid first name`);
-      process.exit(1);
-    }
-    employee.firstName = firstName;
+  case 'search-by-id':
+    searchById();
+    break;
 
-    let lastName = prompt("Last Name: ");
-    if (!lastName) {
-      console.error(`Invalid last name`);
-      process.exit(1);
-    }
-    employee.lastName = lastName;
-
-    let startDateYear = Number(prompt("Employee Start Year (1990-2023): "));
-    if (!Number.isInteger(startDateYear) || startDateYear < 1990 || startDateYear > 2023) {
-      console.error(`Enter a valid start date year within the range (1990-2023)`);
-      process.exit(1);
-    }
-
-    let startDateMonth = Number(prompt("Employee Start Date Month (1-12): "));
-    if (!Number.isInteger(startDateMonth) || startDateMonth < 1 || startDateMonth > 12) {
-      console.error(`Enter a valid start date month (1-12)`);
-      process.exit(1);
-    }
-
-    let startDateDay = Number(prompt("Employee Start Date Day (1-31): "));
-    if (!Number.isInteger(startDateDay) || startDateDay < 1 || startDateDay > 31) {
-      console.error(`Enter a valid start date day (1-31)`);
-      process.exit(1);
-    }
-
-    employee.startDate = new Date(startDateYear, startDateMonth - 1, startDateDay);
-
-    let isActive = prompt("Is employee active (yes or no): ").toLowerCase();
-    if (isActive !== "yes" && isActive !== "no") {
-      console.error(`Enter yes or no for employee active status`);
-      process.exit(1);
-    }
-    employee.isActive = (isActive === "yes");
-
-    console.log(`Employee Added:`, JSON.stringify(employee, null, 2));
+  case 'search-by-name':
+    searchByName();
     break;
 
   default:
-    console.error("Unsupported command. Use 'list' or 'add'.");
+    console.log('Unsupported command. Exiting...');
     process.exit(1);
+
 }
+
+
+
